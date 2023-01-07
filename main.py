421b9aebedaa05a048c8583cc162e906e132f3d5
Bruno Trotti <bruno.trotti@g.globo>
2023-01-07 18:48:18 -0300

    stable

--- main.py
+++ main.py
@@ -5,33 +5,39 @@ from corpus import Corpus
 from utils import *
 
 #Constantes
-G           = .01   # Constante de gravidade
-Ms          = 2.    #Massa do corpo maior
-Me          = 5.    #Massa do corpo menor
-AU          = 2     #Distancia entre os corpos          
-e_ap_v      = .02   #velocidade inicial do corpo no eixo y            
-
+h           = 0.01  #Step size for runge kutta
+G           = .01   # Constante de gravidade  CONDIÇÂO DE EQUILIBRIO ACHADA: .01 
+Ms          = 2.    #Massa do corpo maior CONDIÇÂO DE EQUILIBRIO ACHADA:2.
+Me          = 5.    #Massa do corpo menor CONDIÇÂO DE EQUILIBRIO ACHADA:5.
+AU          = 2.    #Distancia entre os corpos CONDIÇÂO DE EQUILIBRIO ACHADA:2
+e_ap_v      = .01   #velocidade inicial do corpo no eixo y CONDIÇÂO DE EQUILIBRIO ACHADA: .01   
+debbuging = False
 #Corpus
 A = Corpus(np.array([0.,0.]),np.array([0.,0.]),Ms) #Corpo imóvel
 B = Corpus(np.array([AU,0.]),np.array([0.,e_ap_v]),Me) #Corpo móvel
 
-timespace =  230#Aumentar o número de amostras
+timespace =  200.0#Aumentar o número de amostras
 pos = np.array([B.pos])
 
-for i in range(timespace):
+for i in range(int(timespace/h)):
+
     vdist = B.pos - A.pos #Vetor distancia
     dist = np.linalg.norm(vdist) #Modulo
     udist = vdist/dist #Vetor unitário
-    h = 0.1
+    
     dist,cntp_vel = rungekutta4(gravitational_acel,[dist,B.cntp_vel],i,h,(G,Ms)) #Distancia no futuro aproximada por Runge kutta
     
     vvel_cntp = udist*cntp_vel #Usa o vetor unitário da distancia * o valor absoluto velocidade pra calcular o vetor velocidade cntp
-    # print('oi ', vvel_cntp)
+
     B.velocity += vvel_cntp
-    # print(B.velocity)
+
     B.pos += B.velocity #Soma ao vetor distancia a variação em função da velocidade inicial
     pos = np.append(pos,[B.pos],axis=0) #Array de valores
-
+    if debbuging:
+        print('Tempo:',i)
+        print('Vetor velocidade centripeta:', vvel_cntp)
+        print('Vetor velocidade absoluta:',B.velocity)
+        print('Posição:', B.pos)
 # print(pos[0:10])
 plt.plot(pos[:,0], pos[:,1])
 plt.scatter(*A.pos,c='y')
